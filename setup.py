65bdbfb1ec68c5f4d5c3f8759deabfe92f224f34
Hideo Hattori <hattori-h@klab.com>
2015-01-04 17:19:15 +0900

    add setups

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='slowfast',
+    version='0.1',
+    url='https://github.com/hhatto/slowfast',
+    license='MIT',
+    author='Hideo Hattori',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+    ],
+    py_modules=['slowfast'],
+)
